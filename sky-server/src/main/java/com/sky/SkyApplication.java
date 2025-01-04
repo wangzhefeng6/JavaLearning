c064231d2b67867d1338fd1976a7dda9c1819356
wangfuqiang <2275603238@qq.com>
2025-01-04 11:29:18 +0800

    公共字段自动填充

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
+@SpringBootApplication(scanBasePackages = "com.sky")
+
 public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
